32fb269b8c71cf01555c8a9cf546983ecf20efe3
Rafael Jesus <04rafaelpereira@gmail.com>
2025-01-21 06:54:21 -0300

    documentação das classes

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,5 +1,5 @@
 class Avaliacao:
-
+    '''Representa uma avaliação do cliente para o restaurante'''
     def __init__(self, cliente, nota):
         self._cliente = cliente
         self._nota = nota
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,17 @@
 from modelos.avaliacao import Avaliacao
 
 class Restaurante:
+    '''Representa um restaurante e suas características'''
+
     restaurantes = []
 
     def __init__(self, nome, categoria):
+        '''Inicializa uma instância de Restaurante
+        
+        Parâmetros:
+        - Nome (str): nome do restaurante
+        - Categoria (str): categoria do restaurante'''
+
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
@@ -11,23 +19,28 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
+        '''Cria uma representação em string do restaurante'''
         return f'{self._nome} | {self._categoria} | {self._ativo}'
 
     @classmethod
     def listar_restaurantes(cls):
+        '''Exibe uma lista formatada de todos os restaurantes disponíveis'''
         for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(15)} | {restaurante._categoria.ljust(15)} | {str(restaurante.media_avaliacoes).ljust(15)} | {restaurante.ativo}')
             
     @property
     def ativo(self):
+        '''Retorna um símbolo de acordo com o status do restaurante'''
         return '✔️' if self._ativo else '✖️'
     
     def receber_avaliacao(self, cliente, nota):
+        '''Registra uma avaliação para o restaurante'''
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
 
     @property
     def media_avaliacoes(self):
+        '''Calcula e retorna a média das avaliações dos restaurantes'''
         if not self._avaliacao:
             return '-'
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
@@ -36,4 +49,5 @@ class Restaurante:
         return media
 
     def alternar_status(self):
+        '''Altera o status do restaurante'''
         self._ativo = not self._ativo
